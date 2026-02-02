38a80c70cd917e9b46d7a0d314083c77ed44b459
popboxmusic <kyazar07@gmail.com>
2026-02-02 10:08:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,63 +1,84 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {
   getFirestore, collection, addDoc, onSnapshot,
-  query, orderBy, setDoc, doc
+  query, orderBy, setDoc, doc, getDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCrn_tXJZCAlKhem45aXxj4f0h26EPOQ70",
   authDomain: "popboxmusicchat.firebaseapp.com",
   projectId: "popboxmusicchat",
-  storageBucket: "popboxmusicchat.firebasestorage.app",
-  messagingSenderId: "206625719024",
-  appId: "1:206625719024:web:d28f478a2c96d10412f835"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const nick = localStorage.getItem("nick");
-nickLabel.innerText = "Nick: " + nick;
+let currentNick, activePM=null;
+const ADMIN=["popboxmusic","popbox"];
 
-await setDoc(doc(db,"online",nick),{nick});
+loginBtn.onclick = async ()=>{
+  const n=nick.value.trim();
+  const p=pass.value.trim();
+  if(!n||!p) return;
 
-const chat = document.getElementById("chat");
-const users = document.getElementById("users");
+  const ref=doc(db,"users",n);
+  const snap=await getDoc(ref);
 
-function render(d){
-  chat.innerHTML += `<div><b onclick="ozelBaslat('${d.nick}')">${d.nick}</b>: ${d.text}</div>`;
-  chat.scrollTop = chat.scrollHeight;
+  if(snap.exists()){
+    if(snap.data().pass!==p) return alert("Şifre yanlış");
+  }else{
+    await setDoc(ref,{pass:p,role:ADMIN.includes(n)?"admin":"user"});
+  }
+
+  currentNick=n;
+  login.style.display="none";
+  appDiv.style.display="block";
+  baslat();
+};
+
+async function baslat(){
+  await setDoc(doc(db,"online",currentNick),{nick:currentNick});
+
+  send.onclick=gonder;
+  msg.addEventListener("keypress",e=>{if(e.key==="Enter")gonder();});
+
+  const q=query(collection(db,"messages"),orderBy("time"));
+  onSnapshot(q,s=>{
+    chat.innerHTML="";
+    s.forEach(d=>{
+      const m=d.data();
+      chat.innerHTML+=`<div><b onclick="pm('${m.nick}')">${m.nick}</b>: ${m.text}</div>`;
+    });
+  });
+
+  onSnapshot(collection(db,"online"),s=>{
+    users.innerHTML="<b>Online</b><br>";
+    s.forEach(d=>{
+      users.innerHTML+=`<div onclick="pm('${d.data().nick}')">${d.data().nick}</div>`;
+    });
+  });
 }
 
-sendBtn.onclick = async ()=>{
-  const text = msg.value.trim();
-  if(!text) return;
-  await addDoc(collection(db,"messages"),{nick,text,time:Date.now()});
-  msg.value="";
-};
-
-msg.addEventListener("keypress",e=>{
-  if(e.key==="Enter") sendBtn.click();
-});
-
-const q = query(collection(db,"messages"),orderBy("time"));
-onSnapshot(q,(s)=>{
-  chat.innerHTML="";
-  s.forEach(d=>render(d.data()));
-});
-
-onSnapshot(collection(db,"online"),(s)=>{
-  users.innerHTML="<b>Online</b><br>";
-  s.forEach(d=>{
-    users.innerHTML+=`<div onclick="ozelBaslat('${d.data().nick}')">${d.data().nick}</div>`;
+async function gonder(){
+  if(!msg.value) return;
+  await addDoc(collection(db,"messages"),{
+    nick:currentNick,text:msg.value,time:Date.now()
   });
-});
+  msg.value="";
+}
 
-window.ozelBaslat=(n)=>{
-  privateBox.style.display="flex";
-  privateTitle.innerText=n;
+window.pm=(n)=>{
+  if(n===currentNick) return;
+  activePM=n;
+  pmBox.style.display="flex";
+  pmTitle.innerText=n;
 };
 
-window.ozelGonder=()=>{
-  alert("Özel mesaj sonraki adımda");
+pmSend.onclick=async ()=>{
+  if(!pmMsg.value && !imgSend.files[0]) return;
+  await addDoc(collection(db,"pm"),{
+    from:currentNick,to:activePM,
+    text:pmMsg.value,time:Date.now()
+  });
+  pmMsg.value="";
 };
