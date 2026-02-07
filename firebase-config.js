b26d72efb2a62f116418e4e2fe9d090c1109f3b4
popboxmusic <kyazar07@gmail.com>
2026-02-07 07:11:33 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js  
+// Bu dosyayı asla public repo'da paylaşma (veya .gitignore ile dışarıda bırak)  
+// Eğer repo public ise, bu dosyayı silip .env benzeri bir yöntem kullan  
+  
+export const firebaseConfig = {  
+  apiKey: "AIzaSyCrn_tXJZCAlKhem45aXxj4f0h26EPOQ70",  
+  authDomain: "popboxmusicchat.firebaseapp.com",  
+  databaseURL: "https://popboxmusicchat-default-rtdb.firebaseio.com",  
+  projectId: "popboxmusicchat",  
+  storageBucket: "popboxmusicchat.firebasestorage.app",  
+  messagingSenderId: "206625719024",  
+  appId: "1:206625719024:web:d28f478a2c96d10412f835",  
+  measurementId: "G-SB1K22FLEX"  
+};  
