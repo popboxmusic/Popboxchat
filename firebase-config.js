bf86bc739bfd89aa2e32b28137153d64b7ab60b0
popboxmusic <kyazar07@gmail.com>
2026-02-10 11:56:46 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,17 @@
-// firebase-config.js - SEN BUNU DOLDUR
+// firebase-config.js - PROJE BİLGİLERİNLE GÜNCELLE
 console.log("🔥 Firebase Config yükleniyor...");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyABCDEFGHIJKLMNOPQRSTUVWXYZ123456",
-    authDomain: "popbox-elite.firebaseapp.com",
-    databaseURL: "https://popbox-elite-default-rtdb.firebaseio.com",
-    projectId: "popbox-elite",
-    storageBucket: "popbox-elite.appspot.com",
-    messagingSenderId: "123456789012",
-    appId: "1:123456789012:web:abcdef1234567890",
-    measurementId: "G-ABCDEFGHIJ"
+    apiKey: "AIzaSyCrn_tXJZCAlKhem45aXjX4f0h26EPOQ70",
+    authDomain: "popboxmusicchat.firebaseapp.com",
+    databaseURL: "https://popboxmusicchat-default-rtdb.firebaseio.com",
+    projectId: "popboxmusicchat",
+    storageBucket: "popboxmusicchat.firebasestorage.app",
+    messagingSenderId: "206625719024",
+    appId: "1:206625719024:web:d28f478a2c96d10412f835",
+    measurementId: "G-SB1K22FLEX"
 };
 
-// Firebase v9 compat kullanımı için
+// Global erişim için
 window.firebaseConfig = firebaseConfig;
 console.log("✅ Firebase Config hazır!");
